696d82ae39ff685dfe6e308cbca8f530d27d669a
tebalelo-Yongseo <tebaleloma@gmail.com>
2026-09-18 17:09:34 +0200

    add assests/ and Update index.html, script.css, style.css

--- script.js
+++ script.js
@@ -1,45 +1,6 @@
 console.log("Hello, builder");
 
 
-
-//welcome Message
-let button = document.querySelector("#welcomeBtn");
-let bmessage = document.querySelector("#bmessage");
-
-button.addEventListener("click", function(){
-
-bmessage.classList.toggle("hidden");
-
-if(bmessage.classList.contains("hidden")){
-   button.textContent = "View Welcome Message";
-
-}else{
-    button.textContent = "Hide Welcome Message";
-}
-
-});
-
-
-
-//project details
-let pButton = document.querySelector("#projectBtn");
-let details = document.querySelector("#details");
-
-pButton.addEventListener("click", function(){
-    
-    details.classList.toggle("hidden");
-    
-    if(details.classList.contains("hidden")){
-         
-        pButton.textContent = "View Project Details";
-    }else{
-        pButton.textContent = "Hide Project Details";
-    }
-
-});
-
-
-
 //contact form
 
 let contactForm = document.querySelector("#contactForm");
@@ -49,6 +10,9 @@ let formMessage = document.querySelector("#formMessage");
 contactForm.addEventListener("submit", function(event){
   
     event.preventDefault();
+
+    contactForm.reset();
+
     formMessage.classList.remove("hidden");
 
 });
